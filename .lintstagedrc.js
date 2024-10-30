cc8c3110f65dba3dc91be75e4775ac6c2f38d09f
Tryvia Sua Frota Mais Inteligente <hiensen.cdl@gmail.com>
2024-10-30 13:28:11 -0300

    Initial jest tests

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  // Lint & Prettify TS and JS files
-  '*.{js,jsx,ts,tsx}': (filenames) => [
-    `prettier --write ${filenames.join(' ')}`,
-    `npm run lint --fix . ${filenames.join(' --file')}`
+  '*.{js,jsx,ts,tsx}': [
+    'next lint --fix --file', 
+    "eslint --ignore-pattern '!**/.lintstagedrc.js'", 
+    "npm test -- --passWithNoTests --maxWorkers=50%"
   ]
-}
+};
---
+++ src/components/test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react'
+import  {Main}  from '.'
+
+describe('<Main />', ()=>{
+    it('should render the heading', () => {
+        //enderiza o componente
+       const {container} = render(<Main />)
+
+        //busca o elemento e verifica a existencia
+        expect(
+            screen.getByRole('heading', {name: /Hello, World!/i})
+    ).toBeInTheDocument()
+
+
+        //gerar snapchot 
+        expect(container.firstChild).toMatchSnapshot()
+})
+})
