210c126ed1743b3f428fa790f383f2e70b23093e
Tryvia Sua Frota Mais Inteligente <hiensen.cdl@gmail.com>
2024-10-30 09:58:52 -0300

    keep calm and commit

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,7 @@
-const path = require('path')
- 
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`
- 
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  // Lint & Prettify TS and JS files
+  '*.{js,jsx,ts,tsx}': (filenames) => [
+    `prettier --write ${filenames.join(' ')}`,
+    `npm run lint --fix . ${filenames.join(' --file')}`
+  ]
 }
