d33f5b3175ac766141fceb24e47144baa40bb810
Tryvia Sua Frota Mais Inteligente <hiensen.cdl@gmail.com>
2024-11-05 12:55:16 -0300

    automating file creations with Plop and adding generator folder

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -1,10 +1,9 @@
 import { render, screen } from '@testing-library/react'
-import { composeStories } from '@storybook/testing-react';
-import * as stories from './stories';
+
 
 import Main from '.'
 
-const { Default } = composeStories(stories);
+
 
 describe('<Main />', () => {
   it('should render the heading', () => {
@@ -38,14 +37,3 @@ describe('<Main />', () => {
 
   
 })
-
-describe('Main Component Stories', () => {
-  it('should render the default story correctly', () => {
-    render(<Default />);
-
-    expect(screen.getByRole('heading', { name: /React Avançado/i })).toBeInTheDocument();
-    expect(
-      screen.getByAltText(/Imagem de um átomo e React Avançado escrito ao lado./i)
-    ).toBeInTheDocument();
-  });
-});
